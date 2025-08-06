3030b4eb2a079a966b1bcf9648673d107e650cd3
Arjun Datta <121390516+ArjunDatta28052004@users.noreply.github.com>
2025-08-06 12:50:51 +0530

    Create 1137. N-th Tribonacci Number.py

---
+++ 1137. N-th Tribonacci Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        m = [0] * 38
+        m[0] = 0
+        m[1] = 1
+        m[2] = 1
+        for i in range(3, n+1):
+            m[i] = m[i-1]+m[i-2]+m[i-3]
+        return m[n]
+
+        
